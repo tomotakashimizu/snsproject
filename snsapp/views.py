4fb80bb17b257d3b38d0409bdd3dc16068a5673d
tomotakashimizu <tomotaka.developer@gmail.com>
2022-04-16 17:27:03 +0900

    update: create user when signup button is clicked

--- snsapp/views.py
+++ snsapp/views.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import User
 
 
 def signupfunc(request):
-    print(request.POST)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
     return render(request, 'signup.html', {'some': 100})
