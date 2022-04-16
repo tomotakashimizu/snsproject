a587992ad7d7c06eb5b1a4e948a93879ed526daa
tomotakashimizu <tomotaka.developer@gmail.com>
2022-04-16 23:33:11 +0900

    add: create SNS model
    to use ImageField
    $ pip install Pillow
    
    $ python manage.py makemigrations
    $ python manage.py migrate

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SnsModel
 
 # Register your models here.
+
+admin.site.register(SnsModel)
--- snsapp/models.py
+++ snsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SnsModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    snsimage = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.TextField()
