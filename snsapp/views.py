227f92153d686baa32f22ad14bfc5ad99afe7f1c
tomotakashimizu <tomotaka.developer@gmail.com>
2022-04-16 16:52:40 +0900

    add: create UI for signup page and print user_object_list
    (Since models.py has not been modified, makemigrations need not be performed.)
    python manage.py migrate
    python manage.py createsuperuser

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def signupfunc(request):
+    object_list = User.objects.all()
+    print(object_list)
+    if request.method == "POST":
+        print('this is post method')
+    else:
+        print('this is not post method')
     return render(request, 'signup.html', {'some': 100})
